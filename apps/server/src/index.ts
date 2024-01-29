911ea762f88e25e6468b742515758a49e5741899
Aditya Khedekar <adityakhedekar98906@gmail.com>
2024-01-30 05:08:21 +0530

    Add cluster functionality for multi-threading

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,8 @@
 import http from "http";
 import { initSocketService, getIo } from "./services/socket";
 import { startMessageConsumer } from "./services/Kafka";
-import { error } from "console";
+import cluster from "cluster";
+import os from "os";
 
 async function init() {
   await startMessageConsumer();
@@ -17,4 +18,25 @@ async function init() {
   );
 }
 
-init();
+if (cluster.isPrimary) {
+  const numCPUs = os.availableParallelism();
+  console.log(`Number of CPUs is ${numCPUs}`);
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case, it is an HTTP server
+  init();
+
+  console.log(`Worker ${process.pid} started`);
+}
