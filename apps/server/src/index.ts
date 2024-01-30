e50dc533df27b17d0f7cd6dbdf39fd197ae19be7
Aditya Khedekar <adityakhedekar98906@gmail.com>
2024-01-30 05:30:22 +0530

    Refactor index.ts file

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,7 @@
 import http from "http";
 import { initSocketService, getIo } from "./services/socket";
 import { startMessageConsumer } from "./services/Kafka";
-import cluster from "cluster";
-import os from "os";
+import { error } from "console";
 
 async function init() {
   await startMessageConsumer();
@@ -18,25 +17,4 @@ async function init() {
   );
 }
 
-if (cluster.isPrimary) {
-  const numCPUs = os.availableParallelism();
-  console.log(`Number of CPUs is ${numCPUs}`);
-  console.log(`Primary ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
-    console.log("Let's fork another worker!");
-    cluster.fork();
-  });
-} else {
-  // Workers can share any TCP connection
-  // In this case, it is an HTTP server
-  init();
-
-  console.log(`Worker ${process.pid} started`);
-}
+init();
